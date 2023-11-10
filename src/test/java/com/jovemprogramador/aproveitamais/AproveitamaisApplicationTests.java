eae803b2b4dd4a0479d5ebd74fc13a90c730a0ea
LucasdaSilvaSantos <100030572+LucasdaSilvaSantos@users.noreply.github.com>
2023-11-10 16:32:35 -0300

    Add files via upload

---
+++ src/test/java/com/jovemprogramador/aproveitamais/AproveitamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovemprogramador.aproveitamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AproveitamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
