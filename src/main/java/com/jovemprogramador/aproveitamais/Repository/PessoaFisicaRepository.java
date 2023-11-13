0804f75fc6c0747edbffb2cfbf5f1755f8b567e6
LucasdaSilvaSantos <ldsantos2004@gmail.com>
2023-11-13 14:33:16 -0300

    fix: "CPF p/ minusculo"

--- src/main/java/com/jovemprogramador/aproveitamais/Repository/PessoaFisicaRepository.java
+++ src/main/java/com/jovemprogramador/aproveitamais/Repository/PessoaFisicaRepository.java
@@ -1,6 +1,5 @@
 package com.jovemprogramador.aproveitamais.Repository;
 
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
