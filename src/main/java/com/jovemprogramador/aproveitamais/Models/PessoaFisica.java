af0a463696b3d93b0aa9ef54103f2d9abc61f893
JoZonnoZoJ <joaojminecraftabreu@gmail.com>
2023-11-13 14:05:29 -0300

    fix: "CPF tenque ser minúsculo nessa poha"

--- src/main/java/com/jovemprogramador/aproveitamais/Models/PessoaFisica.java
+++ src/main/java/com/jovemprogramador/aproveitamais/Models/PessoaFisica.java
@@ -1,14 +1,13 @@
 package com.jovemprogramador.aproveitamais.Models;
 
-import java.util.UUID;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
@@ -16,28 +15,22 @@ import lombok.Data;
 @Table(name = "Pessoa_Fisica")
 public class PessoaFisica {
     
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID codigo;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int codigo;
 
-    @NotBlank
     @Column(nullable = false, unique = false)
     private String nomeCliente;
 
-    @NotBlank
     @Column(nullable = false, unique = true)
-    private long CPF;
+    private String cpf;
 
-    @NotBlank
     @Column(nullable = false, unique = true)
     private String login;
 
-    @NotBlank
     @Column(nullable = false, unique = false)
     private String senha;
 
-    @NotBlank
     @Column(nullable = false, unique = false)
     private String telefone;
 }
