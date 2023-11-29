dc5bd3b74b1a38f53864835a9a1db372b07150db
JoZonnoZoJ <joaojminecraftabreu@gmail.com>
2023-11-29 16:53:05 -0300

    feat: "continuar em casa JoãoZin"

---
+++ src/main/java/com/jovemprogramador/aproveitamais/Repository/PedidosRepository.java
@@ -0,0 +1,12 @@
+package com.jovemprogramador.aproveitamais.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jovemprogramador.aproveitamais.Models.Pedidos;
+
+public interface PedidosRepository extends JpaRepository<Pedidos, Integer>{
+    
+    
+
+
+}
