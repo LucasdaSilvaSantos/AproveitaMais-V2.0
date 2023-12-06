f72cf6795bbfb7df8aa9c89a9f8c662237062f0a
JefDfelipe <fejedamas@hotmail.com>
2023-12-06 15:15:47 -0300

    feat: "..."

--- AproveitaMais-V2.0/src/main/java/com/jovemprogramador/aproveitamais/Controller/IndexController.java
+++ AproveitaMais-V2.0/src/main/java/com/jovemprogramador/aproveitamais/Controller/IndexController.java
@@ -18,6 +18,11 @@ public class IndexController {
 		return "login";
 	}
 
+	@RequestMapping("/loginLojista")
+	public String loginLojista() {
+		return "loginLojista";
+	}
+
 	@RequestMapping("/cadastro")
 	public String cadastroPF() {
 		return "home/cadastro";
